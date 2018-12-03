3709d15ec3837b12bc6b1c6b6105a5eab574ba58
Austin Green <austingreenkansas@gmail.com>
2018-12-02 20:23:37 -0800

    Move styling to styled-components

--- src/components/header.js
+++ src/components/header.js
@@ -1,42 +1,46 @@
-import { Link } from 'gatsby'
-import PropTypes from 'prop-types'
-import React from 'react'
+import { Link } from 'gatsby';
+import PropTypes from 'prop-types';
+import React from 'react';
 
 const Header = ({ siteTitle }) => (
   <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
+    css={`
+      background: rebeccapurple;
+      margin-bottom: 1.45rem;
+    `}
   >
     <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
+      css={`
+        margin: 0 auto;
+        max-width: 960px;
+        padding: 1.45rem 1.0875rem;
+      `}
     >
-      <h1 style={{ margin: 0 }}>
+      <h1
+        css={`
+          margin: 0;
+        `}
+      >
         <Link
           to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
+          css={`
+            color: white;
+            text-decoration: none;
+          `}
         >
           {siteTitle}
         </Link>
       </h1>
     </div>
   </div>
-)
+);
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
-}
+};
 
 Header.defaultProps = {
   siteTitle: '',
-}
+};
 
-export default Header
+export default Header;
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,10 +1,10 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import Helmet from 'react-helmet'
-import { StaticQuery, graphql } from 'gatsby'
+import React from 'react';
+import PropTypes from 'prop-types';
+import Helmet from 'react-helmet';
+import { StaticQuery, graphql } from 'gatsby';
 
-import Header from './header'
-import './layout.css'
+import Header from './header';
+import './layout.css';
 
 const Layout = ({ children }) => (
   <StaticQuery
@@ -30,22 +30,22 @@ const Layout = ({ children }) => (
         </Helmet>
         <Header siteTitle={data.site.siteMetadata.title} />
         <div
-          style={{
-            margin: '0 auto',
-            maxWidth: 960,
-            padding: '0px 1.0875rem 1.45rem',
-            paddingTop: 0,
-          }}
+          css={`
+            margin: 0 auto;
+            max-width: 960px;
+            padding: 0px 1.0875rem 1.45rem;
+            padding-top: 0;
+          `}
         >
           {children}
         </div>
       </>
     )}
   />
-)
+);
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
-}
+};
 
-export default Layout
+export default Layout;
